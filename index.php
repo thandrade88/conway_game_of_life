e579197626778988b6380d22f3908bfcd0d5237e
thales <thandrade88gmail.com>
2018-01-25 20:57:57 +0000

    The game files

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thales
+ * Date: 23/01/2018
+ * Time: 19:58
+ */
+?>
+
+<html>
+<head>
+
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+
+</head>
+<body>
+
+    <button class="btn" data-type="random">Randon</button>
+    <button class="btn" data-type="gun">Gun</button>
+
+    <canvas id="game-grid" style="margin: 40px; border: 1px solid #000000;"></canvas>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="thegame.js"></script>
+</body>
+</html>
